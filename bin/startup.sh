c670305f950cbe4c34168ff03a5d550c672596aa
François KUBLER <francois+github@kubler.org>
2022-10-29 00:20:39 +0200

    feat: disable login autocomplete by default

--- bin/startup.sh
+++ bin/startup.sh
@@ -5,6 +5,14 @@ then
     # Disable the Web UI unless GEOSERVER_ENABLE_WEBUI is set
     JAVA_OPTS="-DGEOSERVER_CONSOLE_DISABLED=true ${JAVA_OPTS:-''}"
     export JAVA_OPTS
+else
+    # Web UI is enabled
+    if [ -z "{GEOSERVER_ENABLE_LOGIN_AUTOCOMPLETE}" ]
+    then
+        # Disable login autocomplete unless GEOSERVER_ENABLE_LOGIN_AUTOCOMPLETE is set
+        JAVA_OPTS="-Dgeoserver.login.autocomplete=off ${JAVA_OPTS:-}"
+        export JAVA_OPTS
+    fi
 fi
 
 java ${JAVA_OPTS} \
